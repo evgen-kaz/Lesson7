4fdec97b9e51f4f35e70ef16649e60b033d29e21
evgen-kaz <141723080+evgen-kaz@users.noreply.github.com>
2025-08-31 23:38:03 +0800

    Update TestData.java

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -50,7 +50,7 @@ public class TestData {
     }
 
     private static int getRandomInt(int min, int max) {
-        return ThreadLocalRandom.current().nextInt(min, max);
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
     public static String getRandomPhone() {
