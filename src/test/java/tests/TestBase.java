bf61084caedaca33d777fea1415ba9db5e8e4fdb
evgen-kaz <141723080+evgen-kaz@users.noreply.github.com>
2025-09-01 13:08:12 +0800

    Update TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,18 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
+    @BeforeAll
+    static void setupBrowserSize() {
+        Configuration.browserSize = "1920x1080";
+    }
+
     @BeforeEach
     void setupConfig() {
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
     }
